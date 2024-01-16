1aaa07f73e6169e58097c25502f5ebecc82e5640
Steveduke15 <145169365+Steveduke15@users.noreply.github.com>
2024-01-16 07:15:15 +0100

    Update _gcLinkInDm.js

--- plugins/_gcLinkInDm.js
+++ plugins/_gcLinkInDm.js
@@ -1,7 +1,7 @@
 export async function all(m) {
 
   if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('join')) && !m.isBaileys && !m.isGroup) {
-   this.sendMessage(m.chat,{text:`👋 Hey @${m.sender.split('@')[0]}, Would you want to add bot to your group?\nContact bot owner by use *.owner* command.\n\n*© ＺＹＲＯＮＩＸ - ＭＤ  |  ２０２４*`.trim()}, {quoted:m});
+   this.sendMessage(m.chat,{text:`👋 Hey @${m.sender.split('@')[0]}, Would you want to add bot to your group?\nContact bot owner by use *.owner* command.\n\n*© SCORPION - ＭＤ  |  ２０２４*`.trim()}, {quoted:m});
    m.react('📌')
 } 
 
